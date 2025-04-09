455cd37f1ff6ac15046e9300e78ff644798fdcdc
Luis Dale <luis.gcodes@gmail.com>
2025-04-08 22:51:13 -0400

    no more gitignores on sql files

--- app/main.py
+++ app/main.py
@@ -7,15 +7,27 @@ templates = Jinja2Templates(directory="templates")
 
 
 facts = ["Loren Ipsum"]
+location_list = {
+    "Hampstead": "2320 Hanover Pike, Suite 6 | Hampstead, MD 21074",
+    "Glen Burnie": "7714 Ritchie Hwy. | Glen Burnie, MD 21061",
+    "Middle River": "118 Carroll Island Rd. | Middle River, MD 21220",
+}
 
 
 @app.get("/", response_class=HTMLResponse)
 def home(req: Request):
     return templates.TemplateResponse(
-        {"request": req}, name="home.html", context={"facts": facts}
+        request=req, name="home.html", context={"facts": facts}
     )
 
 
 @app.get("/locations", response_class=HTMLResponse)
 def locations(req: Request):
-    return
+    return templates.TemplateResponse(
+        request=req, name="locations.html", context={"locations": location_list}
+    )
+
+
+@app.get("/join", response_class=HTMLResponse)
+def join(req: Request):
+    return templates.TemplateResponse(request=req, name="join.html")
