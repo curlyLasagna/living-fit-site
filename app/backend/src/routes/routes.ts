725971a27110232485c81f86f67a21179cfb6a12
Luis Dale <luis.gcodes@gmail.com>
2025-05-04 16:57:06 -0400

    update routes

--- app/backend/src/routes/routes.ts
+++ app/backend/src/routes/routes.ts
@@ -1,9 +1,8 @@
 import type { Router } from 'express';
 import { createRouter } from '../utils/create';
 import userRoutes from './user-routes';
+import locationRoutes from './locations-routes';
 export default createRouter((router: Router) => {
-    // router.use('/admin', adminRoutes);
     router.use('/user', userRoutes);
+    router.use('/locations', locationRoutes)
 });
-
-
